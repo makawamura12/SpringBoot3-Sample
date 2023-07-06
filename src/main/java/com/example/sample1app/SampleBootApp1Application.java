7eb920a9fbdfc5d3f87b522cbfcad1f4002428ff
ma_kawamura_gxp_co_jp <ma.kawamura@gxp.co.jp>
2023-07-06 20:51:02 +0900

    UI Application

--- src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,33 +1,31 @@
 package com.example.sample1app;
 
-import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SampleBootApp1Application implements ApplicationRunner{
 
 	public static void main(String[] args) {
-		// SpringApplication.run(SampleBootApp1Application.class, args);
 		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
 		app.setBannerMode(Mode.OFF);
+		app.setHeadless(false);
 		app.run(args);
 	}
 	
 	@Override
 	public void run(ApplicationArguments args) {
-		System.out.println("+--------------------------------------+");
-		System.out.println("| this is Application Runner progrem.|");
-		System.out.println("+--------------------------------------+");
-		System.out.println(args.getOptionNames());
-		System.out.println(args.getNonOptionArgs());
-		System.out.println(Arrays.asList(args.getSourceArgs()));
+		JFrame frame = new JFrame("Spring Boot Swing App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,200);
+		frame.add(new JLabel("Spring Boot Application."));
+		frame.setVisible(true);
 	}
 
 }
