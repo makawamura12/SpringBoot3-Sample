75e726d29b75c218d13e75c1926a2b62f0beb83b
ma_kawamura_gxp_co_jp <ma.kawamura@gxp.co.jp>
2023-07-09 18:22:06 +0900

    JSONで出力

--- src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,31 +1,76 @@
 package com.example.sample1app;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SampleBootApp1Application implements ApplicationRunner{
+@RestController
+public class SampleBootApp1Application{
+	
+	DataObject[] data = {
+			new DataObject("noname","no email address",0),
+			new DataObject("taro","taro@yamada",39),
+			new DataObject("hanako","hanako@flower",28),
+			new DataObject("sachiko","sachiko@happy",17),
+			new DataObject("aiueo","ai",6)
+	};
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
 		app.setBannerMode(Mode.OFF);
-		app.setHeadless(false);
 		app.run(args);
 	}
 	
-	@Override
-	public void run(ApplicationArguments args) {
-		JFrame frame = new JFrame("Spring Boot Swing App");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300,200);
-		frame.add(new JLabel("Spring Boot Application."));
-		frame.setVisible(true);
+	@RequestMapping("/{num}")
+	public DataObject index(@PathVariable int num) {
+		int n = num < 0 ? 0 : num >= data.length ? 0 :num;
+		return  data[n];
+		
 	}
+}
 
+class DataObject{
+	private String name;
+	private String mail;
+	private int age;
+	
+	public DataObject(String name, String mail, int age) {
+		super();
+		this.name = name;
+		this.mail = mail;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	
 }
