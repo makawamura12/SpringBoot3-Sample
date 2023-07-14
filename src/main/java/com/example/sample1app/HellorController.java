a9f513546d53d958f16938b0551d3ecad48a4d03
ma_kawamura_gxp_co_jp <ma.kawamura@gxp.co.jp>
2023-07-14 23:22:33 +0900

    Model&parameter

--- src/main/java/com/example/sample1app/HellorController.java
+++ src/main/java/com/example/sample1app/HellorController.java
@@ -2,14 +2,19 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HellorController {
 	
-	@RequestMapping()
-	public String index(Model model) {
-		model.addAttribute("msg","これはコントローラーに用意したメッセージです。");
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		int res = 0;
+		for(int i = 1; i <= num; i++) {
+			res += i;
+		}
+		model.addAttribute("msg","total:" + res);
 		return "index";
 	}
 }
