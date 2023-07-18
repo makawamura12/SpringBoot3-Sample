d385ec9c7db956609a797c5ba5ee43830fabbc3f
ma_kawamura_gxp_co_jp <ma.kawamura@gxp.co.jp>
2023-07-18 21:19:20 +0900

    th:if&th:unless

--- src/main/java/com/example/sample1app/HellorController.java
+++ src/main/java/com/example/sample1app/HellorController.java
@@ -1,28 +1,21 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HellorController {
 	
-	@GetMapping("/")
+	private boolean flag = false;
+	
+	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg","HelloController/indexのページです。");
+		flag = !flag;
 		mav.setViewName("index");
+		mav.addObject("flag",flag);
+		mav.addObject("msg","サンプルのメッセージです。");
 		
 		return mav;
-	}
-	
-	@RequestMapping("/other")
-	public String other() {
-		return "redirect:/";
-	}
-	
-	@RequestMapping("/home")
-	public String home() {
-		return "forward:/";
 	}
 }
