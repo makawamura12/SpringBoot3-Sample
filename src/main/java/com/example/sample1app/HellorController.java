33f863d556c1f928ae929591583ea80b383cf37a
ma_kawamura_gxp_co_jp <ma.kawamura@gxp.co.jp>
2023-07-18 22:16:16 +0900

    th:switch

--- src/main/java/com/example/sample1app/HellorController.java
+++ src/main/java/com/example/sample1app/HellorController.java
@@ -1,18 +1,18 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HellorController {
 	
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping("/{eng}")
+	public ModelAndView index(@PathVariable String eng, ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg","データを表示します。");
-		String[] data = new String[] {"One","Two","Three"};
-		mav.addObject("data",data);
+		mav.addObject("msg",eng + "の日本語は？");
+		mav.addObject("eng", eng);
 		return mav;
 	}
 }
