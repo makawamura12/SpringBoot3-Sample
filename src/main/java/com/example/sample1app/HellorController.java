5001179d35ed3398e66d84690d8c9ce78c435148
ma_kawamura_gxp_co_jp <ma.kawamura@gxp.co.jp>
2023-07-18 20:37:13 +0900

    forward&redirect

--- src/main/java/com/example/sample1app/HellorController.java
+++ src/main/java/com/example/sample1app/HellorController.java
@@ -2,8 +2,7 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,35 +10,19 @@ public class HellorController {
 	
 	@GetMapping("/")
 	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg","フォームを送信してください。");
+		mav.addObject("msg","HelloController/indexのページです。");
 		mav.setViewName("index");
+		
 		return mav;
 	}
 	
-	@PostMapping("/")
-	public ModelAndView form(
-			@RequestParam(value="check1",required=false)boolean check1,
-			@RequestParam(value="radio1", required=false)String radio1,
-			@RequestParam(value="select1", required=false)String select1,
-			@RequestParam(value="select2", required=false)String[] select2,
-			ModelAndView mav) {
-		
-		String res = "";
-		try {
-			res = "check:" + check1 + "radio:" + radio1 + "select:" + select1 + "\nselect2:";
-		}catch(NullPointerException e) {}
-		
-		try {
-			res += select2[0];
-			for(int i = 1; i < select2.length; i++) {
-				res += "," + select2[i];
-			}
-		}catch(NullPointerException e) {
-			res += "null";
-		}
-		
-		mav.addObject("msg",res);
-		mav.setViewName("index");
-		return mav;
+	@RequestMapping("/other")
+	public String other() {
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "forward:/";
 	}
 }
