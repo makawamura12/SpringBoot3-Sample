2557998733215d4bfdc5246b2a2c202b1956ece1
ma_kawamura_gxp_co_jp <ma.kawamura@gxp.co.jp>
2023-07-23 19:07:50 +0900

    ModelAttribute

--- src/main/java/com/example/sample1app/HellorController.java
+++ src/main/java/com/example/sample1app/HellorController.java
@@ -2,10 +2,14 @@ package com.example.sample1app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.sample1app.repositories.PersonRepository;
+
+import jakarta.transaction.Transactional;
 
 @Controller
 public class HellorController {
@@ -14,12 +18,19 @@ public class HellorController {
 	PersonRepository repository;
 	
 	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	public ModelAndView index(@ModelAttribute("formModel")Person Person, ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("title","Hello page");
 		mav.addObject("msg","this is JPA sample data.");
 		Iterable<Person> list = repository.findAll();
 		mav.addObject("data",list);
 		return mav;
+	}
+	
+	@PostMapping("/")
+	@Transactional
+	public ModelAndView form(@ModelAttribute("formModel")Person Person, ModelAndView mav) {
+		repository.saveAndFlush(Person);
+		return new ModelAndView("redirect:/");
 	}
 }
